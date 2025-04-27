c7eb3f2b412c9e9ed16d37f957fc72f47eb70825
Ianana1111 <ian52759@gmail.com>
2025-04-27 12:01:30 +0800

    close eslint for fast building

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    // 警告：這會允許即使你的專案有 ESLint 錯誤，也能成功完成生產環境建置
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
